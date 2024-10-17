fbb7419af66e1523464e90bc69b54dfcf1f698e8
Lexi <119694326+Apb8@users.noreply.github.com>
2024-10-17 17:15:38 +0200

    tried to add animation when destroy but having issues with trigger parameter

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public Animator animator;
+    public float destroyDelay = 0.5f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pelotilla"))
+        {
+            animator.Play("RedBrick");
+            StartCoroutine(DestroyBrick());
+            //Destroy(gameObject);
+        }
+    }
+
+    //si no aconsegueixo q funcioni lo de la animacio aixo ho trec
+    private IEnumerator DestroyBrick()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+    }
+}

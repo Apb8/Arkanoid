4c2ab55ec8f92c999c03473ca7097b97746a5dc1
Lexi <119694326+Apb8@users.noreply.github.com>
2024-10-23 00:24:21 +0200

    tried to implement mouse movement for player, bug pending to fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,35 +7,55 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb; //tmb podria moureho pel transformar i no per les fisiques
     private float inputValue;
     public float moveSpeed = 2.5f;
-    private Vector2 direction;
+    //private Vector2 direction; comento pq es del teclado
     Vector2 startPosition;
 
+    public float minX;
+    public float maxX;
 
     // Start is called before the first frame update
     private void Start()
     {
         startPosition = transform.position;
+
+        // Calculo aqui los limites pero tamb puedo hacerlo manualmente en el inspector
+        float halfPlayerWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+        minX = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + halfPlayerWidth;
+        maxX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x - halfPlayerWidth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputValue = Input.GetAxisRaw("Horizontal");
+        Vector3 mousePosition = Input.mousePosition;
 
-        if(inputValue == 1)
-        {
-            direction = Vector2.right;
-        }
-        else if(inputValue == -1)
-        {
-            direction = Vector2.left;
-        }
-        else
-        {
-            direction = Vector2.zero;
-        }
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        rb.AddForce(direction * moveSpeed * Time.deltaTime * 100);
+        ////E teoria no hace falta ya q lo hago desde el inspector constraints freeze, mirarse be aquestes 2 linies
+        //// Restringir el movimiento solo al eje X, y mantener la Y constante
+        Vector2 targetPosition = new Vector2(Mathf.Clamp(worldPosition.x, minX, maxX), transform.position.y);
+        // Mover la barra suavemente hacia la posición del mouse
+        Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
+
+        rb.velocity = direction * moveSpeed;
+
+        ////Para hacerlo con teclado A-D
+        //inputValue = Input.GetAxisRaw("Horizontal");
+
+        //if(inputValue == 1)
+        //{
+        //    direction = Vector2.right;
+        //}
+        //else if(inputValue == -1)
+        //{
+        //    direction = Vector2.left;
+        //}
+        //else
+        //{
+        //    direction = Vector2.zero;
+        //}
+
+        //rb.AddForce(direction * moveSpeed * Time.deltaTime * 100);
     }
 
     public void ResetPlayer()

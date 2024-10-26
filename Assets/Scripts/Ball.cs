4fe2fe1dfedb78eb2f1391f43b33c197d13282ae
Lexi <119694326+Apb8@users.noreply.github.com>
2024-10-26 23:10:49 +0200

    Ultimos retoques ball speed
    not perfect

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,6 +10,7 @@ public class Ball : MonoBehaviour
     Vector2 startPosition;
     public float speedIncrement = 1.001f; //0.1%
     public float maxSpeed = 350.0f;
+    public float minYVelocity = 0.5f; // velocidad mínima en Y para evitar estancamientos
 
     public AudioSource audioSource;
     public AudioClip brickSound, loseSound, wallSound;
@@ -21,18 +22,32 @@ public class Ball : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        rb.velocity *= speedIncrement;
+        //rb.velocity *= speedIncrement;
 
-        if (rb.velocity.magnitude > maxSpeed)
+        //if (rb.velocity.magnitude > maxSpeed)
+        //{
+        //    rb.velocity = rb.velocity.normalized * maxSpeed;
+        //}
+
+
+        ////evitar que la ball se quede atascada en un mismo mov vertical
+        //if (Mathf.Abs(rb.velocity.x) < 0.5f)
+        //{
+        //    float directionX = rb.velocity.x > 0 ? 0.5f : -0.5f;
+        //    rb.velocity = new Vector2(directionX, rb.velocity.y).normalized * rb.velocity.magnitude;
+        //}
+
+        // Solo incrementa la velocidad si está por debajo del límite
+        if (rb.velocity.magnitude < maxSpeed)
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            rb.velocity *= speedIncrement;
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
         }
 
-
-        //evitar que la ball se quede atascada en un mismo mov vertical
-        if (Mathf.Abs(rb.velocity.x) < 0.5f)
+        // Evitar que la bola se quede atascada en movimiento vertical recto
+        if (Mathf.Abs(rb.velocity.x) < minYVelocity)
         {
-            float directionX = rb.velocity.x > 0 ? 0.5f : -0.5f;
+            float directionX = rb.velocity.x > 0 ? minYVelocity : -minYVelocity;
             rb.velocity = new Vector2(directionX, rb.velocity.y).normalized * rb.velocity.magnitude;
         }
 
